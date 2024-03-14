7d17ee20734711d892a20da5cf858515f42ab145
JoseMan08 <josemanuelberrelleza@gmail.com>
2024-03-13 22:03:20 -0700

    Guardar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,16 @@
 import logo from './chimp.jpg';
 import './App.css';
-import { Button, Form } from 'bootstrap';
+import { Button, Form } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
+        <Form.Label>Nombre : </Form.Label>
+        <Form.Control type="text"></Form.Control>
+        <br />
+        <Button className="App-Button">Guardar</Button>
       </header>
     </div>
   );
