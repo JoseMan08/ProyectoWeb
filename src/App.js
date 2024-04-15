6744e14be5227ac93d555ad6f1efcfaea7949e42
JoseMan08 <josemanuelberrelleza@gmail.com>
2024-04-14 20:24:07 -0700

    Boton Cancelar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import { useState } from 'react';
 import logo from './chimp.jpg';
 import './App.css';
-import SegundoFormulario from './Componentes/SegundoFormulario';
+import Cambiar from './Componentes/Cambiar';
 
 
 function App() {
-  const [personaregistro] = useState ();
+  const [personaregistro] = useState();
 
   return (
-   
-   <div className="App">
+
+    <div className="App">
       <div className="titulo">
+        <img src={logo} className="App-logo" alt="logo" />
       </div>
       <div className="App-header">
-        </div>
-        <div className="logo">
-        </div>
-        <SegundoFormulario personaregistro={personaregistro}/>
-        </div>
-        
+      </div>
+      <div className="logo">
+      </div>
+      <Cambiar/>
+    </div>
+
   )
 }
 
